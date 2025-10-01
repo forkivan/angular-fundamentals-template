935b8d00d3b318328d179076cda1d9f813061240
forkivan <diorg99@gmail.com>
2025-10-01 22:47:38 +0300

    +

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable, of } from "rxjs";
 import { catchError, map } from "rxjs/operators";
 
@@ -46,25 +46,13 @@ export class CoursesService {
   }
 
   filterCourses(value: string): Observable<Course[]> {
-    const search = (value || "").trim().toLowerCase();
-    if (!search) {
-      return this.getAll().pipe(
-        map(res => res.result || []),
+    const params = new HttpParams().set('title', value);
+    return this.http
+      .get<any>(`${this.apiUrl}/courses/filter`, { params })
+      .pipe(
+        map(res => Array.isArray(res) ? res : (res.result || [])),
         catchError(() => of([]))
       );
-    }
-    return this.getAll().pipe(
-      map(res => res.result || []),
-      map(list =>
-        list.filter(
-          (c: Course) =>
-            !!c &&
-            typeof c.title === "string" &&
-            c.title.toLowerCase().includes(search)
-        )
-      ),
-      catchError(() => of([]))
-    );
   }
 
   getAllAuthors(): Observable<any> {
